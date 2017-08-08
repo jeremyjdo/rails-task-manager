1fcce0477d670626a1899020ff0aa43ca86a1927
jeremy jodeau <jeremy.jodeau@gmail.com>
2017-08-08 16:32:32 +0200

    task manager rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  put '/tasks/:id/mark_as_done' => 'tasks#mark_as_done', :as => "mark_as_done"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
